6255385f1c1d6c0bd07d8c66bbabaac0c374f186
ProspectPyxis <prospectpyxis@gmail.com>
2021-06-17 19:00:05 +0700

    feat: new config options for setup timeouts

--- src/interfaces/IConfig.ts
+++ src/interfaces/IConfig.ts
@@ -3,11 +3,15 @@ export interface IConfig {
 	prefix: string
 	timeout: number
 	timeoutWarn: number
+	setupTimeout: number
+	setupTimeoutWarn: number
 }
 
 export const DefaultConfig: IConfig = {
 	prefix: "gm!",
 	timeout: 90,
-	timeoutWarn: 15
+	timeoutWarn: 15,
+	setupTimeout: 120,
+	setupTimeoutWarn: 15
 }
 
